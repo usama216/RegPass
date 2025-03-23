8af40f44d278127eff6792772b16ef70f25c8eda
dev016binarybrix <99108743+usama216@users.noreply.github.com>
2025-03-24 03:51:08 +0500

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // <-- Disable ESLint checks during production build
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
